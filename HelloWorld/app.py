5d9fea5c94a1d1d660cfce035c293d9a866df417
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-07-10 17:12:28 +0530

    variables & recieving input

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,10 +1,12 @@
 print("Hello World!")
 
+# variables
 age = 20
 price = 17.07
 first_name = "sage"
 is_online = False
 print(age, price, first_name, is_online)
 
+# Receiving Input
 name = input("What is your name? ")
 print("Hello " + name)
