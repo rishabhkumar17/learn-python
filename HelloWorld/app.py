b6ae10cd8d9058db0cc2dc17f6b3669aefc6fe6e
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-07-11 17:31:38 +0530

    type conversion

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,12 +1,31 @@
 print("Hello World!")
 
 # variables
-age = 20
-price = 17.07
-first_name = "sage"
-is_online = False
-print(age, price, first_name, is_online)
+# age = 20
+# price = 17.07
+# first_name = "sage"
+# is_online = False
+# print(age, price, first_name, is_online)
 
 # Receiving Input
-name = input("What is your name? ")
-print("Hello " + name)
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# Type Conversion
+# birth_year = input("Enter your Birth Year: ")
+# age = 2023- int(birth_year)
+# print(age)
+
+# int()
+# float()
+# bool()
+# str()
+
+a = input("First: ")
+b = input("Second: ")
+print("sum: ", float(a) + float(b))
+print("sum: " + str(float(a) + float(b)))
+
+
+
+
