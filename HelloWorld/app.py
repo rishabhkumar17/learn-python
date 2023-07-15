52798411e8f2230b9ead9ec402e340ba266d480c
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-07-15 17:59:17 +0530

    if statements

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -61,10 +61,23 @@
 # print(x)
 
 # Logical Operators
-price = 5
-print(price > 10 and price < 30)
-print(price > 10 or price < 30)
-print(not price > 10)
+# price = 5
+# print(price > 10 and price < 30)
+# print(price > 10 or price < 30)
+# print(not price > 10)
+
+# If Statements
+temperature = 7
+if temperature > 30:
+    print("Hot day!")
+elif temperature > 20:
+    print("Nice day!")
+elif temperature > 10:
+    print("Bit cold!")
+else:
+    print("Cold day!")
+print("Done!")
+
 
 
 
